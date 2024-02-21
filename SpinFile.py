38341abc84d63ab77c58b269c414bf79073a95cd
Luxorbris <ahasha222@gmail.com>
2024-02-20 18:39:18 -0700

    Added the readInput method and made main call it three times, then format it

--- SpinFile.py
+++ SpinFile.py
@@ -1,8 +1,8 @@
 from Spinner import *
 def main():
-    i = input("")
+    i = input("").lower()
     s = Spinner()
-    s.makeDictionary("test-synonyms.txt")
-    print(s.synonyms)
+    s.makeDictionary("synonyms-simplified.txt")
+    print (f"Original: {i}\nOption 1: {s.readInput(i)}\nOption 2: {s.readInput(i)}\nOption 3: {s.readInput(i)}")
 
 main()
--- Spinner.py
+++ Spinner.py
@@ -1,4 +1,5 @@
-
+from string import *
+from random import *
 class Spinner:
     def __init__ (self):
         self.synonyms = {}
@@ -13,4 +14,22 @@ class Spinner:
                 values.append(line[y].strip())
             self.synonyms[k] = values
     def readInput(self,i):
-        i.strip()
+        wordList = []
+        self.i = i.translate(str.maketrans(" "," ",punctuation))
+        p = i.split(" ")
+        for r in range(len(p)):
+            change = False
+            for s in self.synonyms.keys():
+                if p[r] == s:
+                    randomNumber = randint(1,100)
+                    if randomNumber >= 50:
+                        change = True
+                        choices = (self.synonyms[p[r]])
+                        randomChoice = randint(0,len(choices))
+                        wordList.append(choices[randomChoice])
+                        break
+            if change == False:
+                wordList.append(p[r])
+        spun = " ".join(wordList)
+        return (spun)
+
