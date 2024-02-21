2851b02793f26c17ff7675943964662ecf3fb003
Luxorbris <ahasha222@gmail.com>
2024-02-20 18:58:48 -0700

    Made comments for the code

--- SpinFile.py
+++ SpinFile.py
@@ -1,10 +1,10 @@
 from Spinner import *
 def main():
-    e = open("essay.txt","r")
-    i = e.read()
-    s = Spinner()
-    s.makeDictionary("synonyms-simplified.txt")
+    e = open("essay.txt","r") # opens the essay
+    i = e.read() # assigns the entire essay string to i
+    s = Spinner() # makes a new object
+    s.makeDictionary("synonyms-simplified.txt") # converts the synonym file into a dictionary
     print (f"Original: {i}\nOption 1: {s.readInput(i)}\nOption 2: {s.readInput(i)}\nOption 3: {s.readInput(i)}")
-
+    # ^ makes and formats different versions into the original as well as three options
 if __name__ == "__main__":
-    main()
+    main() # starts the program
--- Spinner.py
+++ Spinner.py
@@ -2,34 +2,34 @@ from string import *
 from random import *
 class Spinner:
     def __init__ (self):
-        self.synonyms = {}
+        self.synonyms = {} # makes the synonym dictionary callable
     def makeDictionary(self, file):
-        f = open(file,"r")
-        for x in f:
-            values = []
-            l = x.replace(":",",")
-            line = l.split(",")
-            k = line[0]
+        f = open(file,"r") # opens the file sent into the method
+        for x in f: # reads each line
+            values = [] # makes the values list and empties the old one
+            l = x.replace(":",",") # makes the words easier to split
+            line = l.split(",") # splits the words
+            k = line[0] # assigns the first word as a key
             for y in range(1,len(line)):
-                values.append(line[y].strip())
-            self.synonyms[k] = values
+                values.append(line[y].strip()) # strips and adds the word to the values list
+            self.synonyms[k] = values # appends the key and values into the synonym dictionary
     def readInput(self,i):
-        wordList = []
-        self.i = i.translate(str.maketrans(" "," ",punctuation))
-        p = i.split(" ")
-        for r in range(len(p)):
-            change = False
-            for s in self.synonyms.keys():
-                if p[r] == s:
-                    randomNumber = randint(1,100)
-                    if randomNumber >= 50:
-                        change = True
-                        choices = (self.synonyms[p[r]])
-                        randomChoice = randint(0,len(choices)-1)
-                        wordList.append(choices[randomChoice])
+        wordList = [] # makes the list and empties the old one
+        self.i = i.translate(str.maketrans(" "," ",punctuation)) # removes punctuation
+        p = i.split(" ") # splits the entire thing into words based on spaces
+        for r in range(len(p)): # goes through the whole "essay"
+            change = False # automatically sets change back to false at the beginning of each loop
+            for s in self.synonyms.keys(): # goes through the previously made dictionary
+                if p[r] == s: # checks the word against the dictionary keys
+                    randomNumber = randint(1,100) # chooses a random number on a match
+                    if randomNumber >= 50: # if the random number is 50 or more, the word changes
+                        change = True # lets the list know later on not to add an additonal word
+                        choices = (self.synonyms[p[r]]) # takes the values from the dictionary keys
+                        randomChoice = randint(0,len(choices)-1) # chooses a random word's value from the dictionary values
+                        wordList.append(choices[randomChoice]) # puts the chosen word into the list
                         break
-            if change == False:
+            if change == False: # if the word doesn't change, the normal word gets added to the list
                 wordList.append(p[r])
-        spun = " ".join(wordList)
-        return (spun)
+        spun = " ".join(wordList) # fuses the whole thing back into one "essay" for easy printing
+        return (spun) # sends back the changed "essay"
 
